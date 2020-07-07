d62b518860a2b952948e591c931d8f5803ad5b47
Alex Lanzano <lanzano.alex@gmail.com>
2020-07-07 08:19:00 -0400

    boot: load kernel in the higher half of 4GiB

--- boot/startc.c
+++ boot/startc.c
@@ -2,7 +2,8 @@
 #include "memory.h"
 #include "asm.h"
 
-#define KERNEL_MAIN 0x8784
+#define KERNEL_MAIN_PHYS 0x87a4
+#define KERNEL_MAIN_VIRT 0xc0100000 + KERNEL_MAIN_PHYS
 
 void startc()
 {
@@ -12,7 +13,8 @@ void startc()
     page_map = alloc_page();
 
     /* Map bootloader and kernel */
-    map_range(page_map, 0x0, 0x0, 512);
+    map_range(page_map, 0x0, 0x0, 12);
+    map_range(page_map, 0x0, 0xc0100000, 12);
 
     /* Map VGA Framebuffer */
     map_range(page_map, 0xB8000, 0xB8000, 8);
@@ -21,5 +23,5 @@ void startc()
 	enable_pae();
     enable_long_mode();
 	enable_paging();
-	ljmp(0x18, KERNEL_MAIN);
+	ljmp(0x18, KERNEL_MAIN_VIRT);
 }
